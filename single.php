46a1bb71d11cc93624867cb39ab53a0024f4dc2b
nalzai35 <zainal.jelek@yahoo.com>
2018-05-28 20:03:22 +0700

    edit

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
     <div class="row">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <article class="single mb-4 clearfix col-12">
-            <h1 class="mb-3 text-capitalize"><?php the_title(); ?></a></h1>
+            <h1 class="mb-3 h4 text-capitalize"><?php the_title(); ?></a></h1>
             <?php if ( has_post_thumbnail() ) : ?>
                 <div class="mb-3"><?php the_post_thumbnail('large', array('class' => 'img-fluid')); ?></div>
             <?php endif; ?>
